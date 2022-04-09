4a9be7c71c91ef2f86e99a6c19c813895fb5513b
varshith v hegde <varshithvh@gmail.com>
2022-04-09 21:45:03 +0530

    new features

--- app/src/main/java/com/varshith/sampledemo/MainActivity.kt
+++ app/src/main/java/com/varshith/sampledemo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.varshith.sampledemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         var message: String? = intent.getStringExtra("message_key")
         (supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment)
             .setOnTapPlaneGlbModel(message)
+        val bu: Button = findViewById(R.id.back)
+        bu.setOnClickListener {
+            val intent = Intent(this, index::class.java)
+
+            startActivity(intent)
+        }
     }
 }
 //https://drive.google.com/uc?export=download&id=15oISqz_WDp6bteffyG4wa48q-R10-yRM
--- app/src/main/java/com/varshith/sampledemo/index.kt
+++ app/src/main/java/com/varshith/sampledemo/index.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
 
 class index : AppCompatActivity() {
 
@@ -14,6 +15,11 @@ class index : AppCompatActivity() {
         var b : String="https://drive.google.com/uc?export=download&id=15oISqz_WDp6bteffyG4wa48q-R10-yRM"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
+        this.supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        // Displaying the custom layout in the ActionBar
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+        supportActionBar!!.setCustomView(R.layout.my_layout)
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
